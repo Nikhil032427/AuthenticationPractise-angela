f462964625beab8349e1e67607fd4aad68da0769
Nikhil032427 <nikhilkumarreddymukku03@gmail.com>
2022-12-22 15:42:35 +0530

    Level 2 With .env file.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -25,8 +26,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "encryptionUsingMongooseEncryptionPlugin";
-userSchema.plugin(encrypt,{secret:secret, encryptedFields: ["password"]});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User",userSchema);
 
