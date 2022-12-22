d7e11cab66cb45b8120f012911ec18aebd343160
Nikhil032427 <nikhilkumarreddymukku03@gmail.com>
2022-12-22 14:20:21 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDb', {
+    //  userNewUrlParser:true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected!!!!!');
+}).catch((e) => {
+    console.log(e);
+});
+
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User",userSchema);
+
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+
+app.get("/register",(req,res)=>{
+    res.render("register");
+});
+
+app.post("/register",(req,res)=>{
+const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+});
+newUser.save((err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        res.render("secrets");
+    }
+})
+});
+
+app.post("/login",(req,res)=>{
+    const username =   req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, (err, foundUser)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser.password === password){
+                res.render("secrets");
+            }
+        }
+    })
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Server Started on Port 3000.");
+})
