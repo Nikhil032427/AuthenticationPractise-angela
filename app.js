6ea5f6bd8522cd1cdf56b798746001708a770fa3
Nikhil032427 <nikhilkumarreddymukku03@gmail.com>
2023-01-05 11:48:05 +0530

    Level 4 with bcypt and salting.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 //const encrypt = require("mongoose-encryption");
 const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -45,30 +47,39 @@ app.get("/register",(req,res)=>{
 });
 
 app.post("/register",(req,res)=>{
-const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-});
-newUser.save((err)=>{
-    if(err){
-        console.log(err);
-    }else{
-        res.render("secrets");
-    }
-})
+// usnig bcrypt and salting to make passwords more secure
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save((err)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.render("secrets");
+            }
+        })
+    });
 });
 
 app.post("/login",(req,res)=>{
     const username =   req.body.username;
-    const password = md5(req.body.password);
+  //  const password = md5(req.body.password);
+    const password = req.body.password;
    
     User.findOne({email: username}, (err, foundUser)=>{
         if(err){
             console.log(err);
         }else{
-            if(foundUser.password === password){
-                res.render("secrets");
-            }
+            // if(foundUser.password === password){
+            //     res.render("secrets");
+            // }
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if(result === true){
+                    res.render("secrets");
+                }
+            });
         }
     })
 
